2bbe9ff781eec67bef1afc5cd4d1ca8c9b4ac321
JuanPavi25 <juancamilopavi9@gmail.com>
2026-02-23 21:32:50 -0500

    .

--- src/app/ui/acme-logo.jsx
+++ src/app/ui/acme-logo.jsx
@@ -4,15 +4,15 @@ import { lusitana } from '@/app/ui/fonts';
 
 export default function AcmeLogo() {
   return (
-    <Link href="/" legacyBehavior>
-      <a className="flex items-center gap-2 group">
-        <div className={`${lusitana.className} text-white`}>
-          <DocumentTextIcon className="h-12 w-12 rotate-[15deg]" />
-        </div>
-        <span className="font-bold text-xl tracking-tight text-white group-hover:text-blue-100 transition-colors">
-          Factura <span className="font-light">App</span>
-        </span>
-      </a>
-    </Link>
+
+    <a className="flex items-center gap-2 group">
+      <div className={`${lusitana.className} text-white`}>
+        <DocumentTextIcon className="h-12 w-12 rotate-[15deg]" />
+      </div>
+      <span className="font-bold text-xl tracking-tight text-white group-hover:text-blue-100 transition-colors">
+        Factura <span className="font-light">App</span>
+      </span>
+    </a>
+
   );
 }
